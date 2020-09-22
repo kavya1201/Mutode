f10b564b947f52abb96c1a6e55b6f11d258433e0
kavya1201 <56418793+kavya1201@users.noreply.github.com>
2020-09-21 22:21:55 -0500

    Add files via upload

---
+++ killed.js
@@ -0,0 +1,120 @@
+const string = require('./killed-dep')
+
+module.exports = {
+  deletion () {
+    return true
+  },
+  math (n, m) {
+    let a = n + m
+    let b = n - m
+    let c = n * m
+    let d = n / m
+    let e = n % m
+    let f = n | m
+    let g = n & m
+    let h = n ^ m
+    let i = n ** m
+    let j = n << m
+    let k = n >> m
+    return +(a + b + c + d + e + f + g + h + i + j + k).toFixed(1)
+  },
+  increments (a) {
+    a++
+    a--
+    return a
+  },
+  conditionals (a) {
+    if (a === -1) {
+      return 0
+    }
+    if (a < 2) {
+      return a
+    }
+    if (a <= 3) {
+      return a * 2
+    }
+    if (a >= 7) {
+      return a * 3
+    }
+    if (a > 5) {
+      return a * 4
+    }
+    if (a !== 4) {
+      return -1
+    }
+    return a * 5
+  },
+  negatives (a) {
+    return -a
+  },
+  stringLiterals: {
+    hello () {
+      return string
+    },
+    empty () {
+      return ''
+    }
+  },
+  numericLiterals: {
+    zero () {
+      return 0
+    },
+    one () {
+      return 1
+    },
+    ten () {
+      return 10
+    }
+  },
+  booleanLiterals: {
+    booleanTrue () {
+      return true
+    },
+    booleanFalse () {
+      return false
+    }
+  },
+  functions () {
+    const b = function () {
+      return 2
+    }
+
+    function a (p1, p2, p3) {
+      return p1 * p2 * p3
+    }
+
+    return a(1, 2, 3) * b()
+  },
+  arrays () {
+    const a = [1,2,3]
+    const b = [
+      4,
+      5,
+      {
+        a: 6,
+        b: 7
+      }
+    ]
+    return a.concat(b)
+  },
+  objects (bool) {
+    const a = { a: 1, b: 2}
+    const b = {
+      a: 3,
+      b: [
+        4, 5
+      ]
+    }
+    return bool ? a : b
+  },
+  switchCases(a) {
+    switch (a) {
+      case 1:
+        return 2
+      case true:
+        return 3
+      default:
+        return 4
+    }
+  }
+}
---
+++ src/mutantRunner.js
@@ -0,0 +1,52 @@
+const chalk = require('chalk')
+const spawn = require('child_process').spawn
+const Debug = require('debug')
+const fs = require('fs')
+const path = require('path')
+const terminate = require('terminate')
+
+/**
+ * @module MutantRunner
+ * @description Runs a given mutant in a free worker, logging one of the possible results (survived, killed or discarded) and the time of execution.
+ *
+ * Execution is done with the `npm test` command inside the worker's directory
+ */
+module.exports = function MutantRunner ({mutodeInstance, filePath, contentToWrite, log}) {
+  const debug = Debug(`mutants:${filePath}`)
+  return async index => {
+    await new Promise(resolve => {
+      const startTime = process.hrtime()
+      fs.writeFileSync(`.mutode/mutode-${mutodeInstance.id}-${index}/${filePath}`, contentToWrite)
+      const child = spawn(mutodeInstance.npmCommand, ['test'], {cwd: path.resolve(`.mutode/mutode-${mutodeInstance.id}-${index}`)})
+
+      child.stderr.on('data', data => {
+        debug(data.toString())
+      })
+
+      let timedout = false
+      const timeout = setTimeout(() => {
+        terminate(child.pid)
+        timedout = true
+      }, mutodeInstance.timeout).unref()
+
+      child.on('exit', (code, signal) => {
+        const endTime = process.hrtime(startTime)
+        const endTimeMS = (endTime[0] * 1e3 + endTime[1] / 1e6).toFixed(0)
+        const timeDiff = chalk.gray(`${endTimeMS} ms`)
+        clearTimeout(timeout)
+        if (code === 0) {
+          console.log(`${log}\t${chalk.bgRed('survived')} ${timeDiff}`)
+          mutodeInstance.survived++
+        } else if (signal || timedout) {
+          console.log(`${log}\t${chalk.bgBlue('discarded (timeout)')} ${timeDiff}`)
+          mutodeInstance.discarded++
+        } else {
+          console.log(`${log}\t${chalk.bgGreen('killed')} ${timeDiff}`)
+          mutodeInstance.killed++
+        }
+        // console.log('exit', code)
+        resolve()
+      })
+    })
+  }
+}
